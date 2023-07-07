934eda3c150bae2a88352ba7da981bdd6013f1f2
Chaitanya-kamra <chaitanyakamra001@gmail.com>
2023-07-07 20:39:47 +0530

    update

---
+++ app/src/main/java/com/chaitanya/filedownloader/ItemAdapter.kt
@@ -0,0 +1,36 @@
+package com.chaitanya.filedownloader
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.chaitanya.filedownloader.databinding.ItemDownloadBinding
+
+class ItemAdapter( private val items: ArrayList<String>,
+                   private val updateListener: (id: Int) -> Unit):
+    RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+    class ViewHolder(binding: ItemDownloadBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        val tvTitle = binding.itemTitle
+        val tvProgress = binding.tvProgress
+        val tvStatus = binding.tvItemStatus
+        val progressBar = binding.itemProgress
+        val ivIcon = binding.ivIcon
+        val ivDelete = binding.ivCross
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemDownloadBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/chaitanya/filedownloader/MainActivity.kt
+++ app/src/main/java/com/chaitanya/filedownloader/MainActivity.kt
@@ -1,28 +1,61 @@
 package com.chaitanya.filedownloader
 
-import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.PopupMenu
 import android.widget.SeekBar
 import android.widget.TextView
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.documentfile.provider.DocumentFile
 import com.chaitanya.filedownloader.databinding.ActivityMainBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import java.net.URL
 
 class MainActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var bottomSheetDialog : BottomSheetDialog
+    private lateinit var bottomSheetView: View
+
+    private var maxParallelDownload : Int = 1
+
+    private var selectedFolder: Uri? = null
+
+    // ActivityResultLauncher for folder selection
+    private val folderSelectionLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                val folderUri: Uri? = data?.data
+                val folderDocument = folderUri?.let { DocumentFile.fromTreeUri(this, it) }
+                val folderName = folderDocument?.name
+                Log.e("path", folderDocument!!.uri.path.toString())
+                selectedFolder = folderUri
+                bottomSheetView.findViewById<EditText>(R.id.etDestination).setText(folderName)
+
+            }
+        }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,10 +68,16 @@ class MainActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
             showMenu(view)
         }
         bottomSheetDialog = BottomSheetDialog(this)
-        val bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_layout, null)
+        bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_layout, null)
         bottomSheetDialog.setContentView(bottomSheetView)
 
 
+        bottomSheetView.findViewById<EditText>(R.id.etDestination).setOnClickListener {
+            openFolderSelection()
+        }
+        bottomSheetView.findViewById<Button>(R.id.addDownloadButton).setOnClickListener {
+            downloadVideo("http://167.99.95.99/c/1.bin")
+        }
 
         val slider = bottomSheetView.findViewById<SeekBar>(R.id.slider)
         val sliderValue = bottomSheetView.findViewById<TextView>(R.id.sliderValue)
@@ -60,10 +99,10 @@ class MainActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
 //        backButton.setOnClickListener {
 //            bottomSheetDialog.dismiss()
 //        }
-//        val addButton = bottomSheetView.findViewById<Button>(R.id.addButton)
-//        addButton.setOnClickListener{
-//            fetchUrlDetails("http://167.99.95.99/a/10.bin")
-//        }
+        val addButton = bottomSheetView.findViewById<Button>(R.id.addLinkButton)
+        addButton.setOnClickListener{
+            fetchUrlDetails("https://ia802607.us.archive.org/27/items/the-kerala-story_202305/BOY%20AND%20THE%20KING%20-%20Abdallah%20Al%20Faisal.jpg")
+        }
 //
 //        val updateButton = bottomSheetView.findViewById<Button>(R.id.updateButton)
 //        updateButton.setOnClickListener {
@@ -71,29 +110,60 @@ class MainActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
 //            // Perform the necessary action to update the parallel download count
 //            // You can add your implementation logic here
 //        }
-        binding.button2.setOnClickListener {
+        binding.buttonAdd.setOnClickListener {
+            linkUi()
             bottomSheetDialog.show()
 
         }
     }
 
-    fun fetchUrlDetails(urlString: String) {
+    private fun fetchUrlDetails(urlString: String) {
+        runOnUiThread{
+            loadingUi()
+        }
         GlobalScope.launch(Dispatchers.IO) {
             val url = URL(urlString)
             val connection = url.openConnection()
             connection.connect()
 
-            val fileSize = connection.contentLength
-            val contentType = connection.contentType
-            val fileName = url.path.substring(url.path.lastIndexOf('/') + 1)
 
-            // Print or use the fetched details as per your requirements
-            println("File Name: $fileName")
-            println("File Size: $fileSize bytes")
-            println("File Type: $contentType")
+            val fileSize = connection.contentLength
+            val fileName = url.path.substring(url.path.lastIndexOf('/') + 1)
+            val name = fileName.substringBeforeLast(".")
+            val extension = fileName.substringAfterLast(".")
+            Log.e("check",fileSize.toString() )
+            runOnUiThread{
+                downloadUi()
+                val editName = bottomSheetView.findViewById<EditText>(R.id.etFileName)
+                val tvFileSize = bottomSheetView.findViewById<TextView>(R.id.tvSize)
+                val tvExtension = bottomSheetView.findViewById<TextView>(R.id.tvExtension)
+                tvExtension.text = extension
+                tvFileSize.text = if (fileSize < 0){
+                    "Unknown"
+                }else {
+                    formatDataSize(fileSize)
+                }
+                editName.setText(name)
+            }
         }
     }
+    private fun formatDataSize(size: Int): String {
+        val units = arrayOf("B", "KB", "MB", "GB", "TB")
+        var fileSize = size.toDouble()
+        var unitIndex = 0
+        while (fileSize > 1024 && unitIndex < units.size - 1) {
+            fileSize /= 1024
+            unitIndex++
+        }
+        return "%.2f %s".format(fileSize, units[unitIndex])
+    }
 
+    private fun openFolderSelection() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        intent.addCategory(Intent.CATEGORY_DEFAULT)
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        folderSelectionLauncher.launch(intent)
+    }
     @RequiresApi(Build.VERSION_CODES.Q)
     private fun showMenu(view: View) {
         PopupMenu(this, view).apply {
@@ -106,14 +176,164 @@ class MainActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
     override fun onMenuItemClick(item: MenuItem?): Boolean {
         return when (item!!.itemId) {
             R.id.action_settings -> {
-                Toast.makeText(this,"setts",Toast.LENGTH_LONG).show()
+                settingUi()
+                bottomSheetDialog.show()
                 true
             }
-
             else -> false
         }
     }
+    private fun downloadVideo(mediaStream: String) {
+        mediaStream.let {
+            val client = OkHttpClient()
+            val request = Request.Builder()
+                .url(it)
+                .header("Connection", "close")
+                .build()
 
+//            val titleVid = binding.tvTitle.text.toString()
+//            val sanitizedNameVid = titleVid.replace(Regex("[^a-zA-Z0-9.-]"), "")
+//            val timestamp = System.currentTimeMillis()
+            val fileName = "ok.bin"
+            val file = File(filesDir, fileName)
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    runOnUiThread { notSuccessUi() }
 
+                    // Handle download failure
+                    Log.e("OKHTTP", "failure")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    response.body?.let { responseBody ->
+                        val inputStream = responseBody.byteStream()
+                        val outputStream = FileOutputStream(file)
+
+                        val buffer = ByteArray(4096)
+                        var bytesRead: Int
+                        var totalBytesRead: Long = 0
+                        val totalBytes = responseBody.contentLength()
+                        var progress: Int
+
+                        try {
+                            while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+                                outputStream.write(buffer, 0, bytesRead)
+                                totalBytesRead += bytesRead
+
+                                progress = ((totalBytesRead.toDouble() / totalBytes) * 100).toInt()
+                                Log.e("progress", progress.toString())
+                                // Update the progress bar and display the downloaded and total data
+                                runOnUiThread {
+//                                    binding.progressBar.progress = progress
+//                                    val downloadedData = formatDataSize(totalBytesRead)
+//                                    val totalData = formatDataSize(totalBytes)
+//                                    binding.tvprogress.text = "$downloadedData / $totalData"
+                                }
+                            }
+                        } catch (e: IOException) {
+
+                            e.printStackTrace()
+                            runOnUiThread { notSuccessUi() }
+                            Log.e("progress", "failure")
+                        } finally {
+                            try {
+                                outputStream.close()
+                                inputStream.close()
+                            } catch (e: IOException) {
+                                // Handle file close error
+                                e.printStackTrace()
+                                runOnUiThread { notSuccessUi() }
+
+                            }
+                        }
+                    }
+                }
+            })
+        }
+    }
+    private fun settingUi(){
+        val settingLayout = bottomSheetView.findViewById<View>(R.id.lnlSetting)
+        val loadingLayout = bottomSheetView.findViewById<View>(R.id.lnlGrabbing)
+        val linkLayout = bottomSheetView.findViewById<View>(R.id.lnlEnterLink)
+        val downloadLayout = bottomSheetView.findViewById<View>(R.id.lnlAddDownload)
+        val successFulLLayout = bottomSheetView.findViewById<View>(R.id.lnlSuccessful)
+        val unSuccessLayout = bottomSheetView.findViewById<View>(R.id.lnlNotSuccessFull)
+        settingLayout.visibility = View.VISIBLE
+        loadingLayout.visibility = View.GONE
+        linkLayout.visibility = View.GONE
+        downloadLayout.visibility = View.GONE
+        successFulLLayout.visibility = View.GONE
+        unSuccessLayout.visibility = View.GONE
+    }
+    private fun loadingUi(){
+        val settingLayout = bottomSheetView.findViewById<View>(R.id.lnlSetting)
+        val loadingLayout = bottomSheetView.findViewById<View>(R.id.lnlGrabbing)
+        val linkLayout = bottomSheetView.findViewById<View>(R.id.lnlEnterLink)
+        val downloadLayout = bottomSheetView.findViewById<View>(R.id.lnlAddDownload)
+        val successFulLLayout = bottomSheetView.findViewById<View>(R.id.lnlSuccessful)
+        val unSuccessLayout = bottomSheetView.findViewById<View>(R.id.lnlNotSuccessFull)
+        settingLayout.visibility = View.GONE
+        loadingLayout.visibility = View.VISIBLE
+        linkLayout.visibility = View.GONE
+        downloadLayout.visibility = View.GONE
+        successFulLLayout.visibility = View.GONE
+        unSuccessLayout.visibility = View.GONE
+    }
+    private fun linkUi(){
+        val settingLayout = bottomSheetView.findViewById<View>(R.id.lnlSetting)
+        val loadingLayout = bottomSheetView.findViewById<View>(R.id.lnlGrabbing)
+        val linkLayout = bottomSheetView.findViewById<View>(R.id.lnlEnterLink)
+        val downloadLayout = bottomSheetView.findViewById<View>(R.id.lnlAddDownload)
+        val successFulLLayout = bottomSheetView.findViewById<View>(R.id.lnlSuccessful)
+        val unSuccessLayout = bottomSheetView.findViewById<View>(R.id.lnlNotSuccessFull)
+        settingLayout.visibility = View.GONE
+        loadingLayout.visibility = View.GONE
+        linkLayout.visibility = View.VISIBLE
+        downloadLayout.visibility = View.GONE
+        successFulLLayout.visibility = View.GONE
+        unSuccessLayout.visibility = View.GONE
+    }
+    private fun downloadUi(){
+        val settingLayout = bottomSheetView.findViewById<View>(R.id.lnlSetting)
+        val loadingLayout = bottomSheetView.findViewById<View>(R.id.lnlGrabbing)
+        val linkLayout = bottomSheetView.findViewById<View>(R.id.lnlEnterLink)
+        val downloadLayout = bottomSheetView.findViewById<View>(R.id.lnlAddDownload)
+        val successFulLLayout = bottomSheetView.findViewById<View>(R.id.lnlSuccessful)
+        val unSuccessLayout = bottomSheetView.findViewById<View>(R.id.lnlNotSuccessFull)
+        settingLayout.visibility = View.GONE
+        loadingLayout.visibility = View.GONE
+        linkLayout.visibility = View.GONE
+        downloadLayout.visibility = View.VISIBLE
+        successFulLLayout.visibility = View.GONE
+        unSuccessLayout.visibility = View.GONE
+    }
+    private fun successUi(){
+        val settingLayout = bottomSheetView.findViewById<View>(R.id.lnlSetting)
+        val loadingLayout = bottomSheetView.findViewById<View>(R.id.lnlGrabbing)
+        val linkLayout = bottomSheetView.findViewById<View>(R.id.lnlEnterLink)
+        val downloadLayout = bottomSheetView.findViewById<View>(R.id.lnlAddDownload)
+        val successFulLLayout = bottomSheetView.findViewById<View>(R.id.lnlSuccessful)
+        val unSuccessLayout = bottomSheetView.findViewById<View>(R.id.lnlNotSuccessFull)
+        settingLayout.visibility = View.GONE
+        loadingLayout.visibility = View.GONE
+        linkLayout.visibility = View.GONE
+        downloadLayout.visibility = View.GONE
+        successFulLLayout.visibility = View.VISIBLE
+        unSuccessLayout.visibility = View.GONE
+    }
+    private fun notSuccessUi(){
+        val settingLayout = bottomSheetView.findViewById<View>(R.id.lnlSetting)
+        val loadingLayout = bottomSheetView.findViewById<View>(R.id.lnlGrabbing)
+        val linkLayout = bottomSheetView.findViewById<View>(R.id.lnlEnterLink)
+        val downloadLayout = bottomSheetView.findViewById<View>(R.id.lnlAddDownload)
+        val successFulLLayout = bottomSheetView.findViewById<View>(R.id.lnlSuccessful)
+        val unSuccessLayout = bottomSheetView.findViewById<View>(R.id.lnlNotSuccessFull)
+        settingLayout.visibility = View.GONE
+        loadingLayout.visibility = View.GONE
+        linkLayout.visibility = View.GONE
+        downloadLayout.visibility = View.GONE
+        successFulLLayout.visibility = View.GONE
+        unSuccessLayout.visibility = View.VISIBLE
+    }
 
 }
